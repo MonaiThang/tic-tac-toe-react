ba0fe614c9058b309e36d36aeafa5fac443210ef
Monai Thangsuphanich <monait@altis.com.au>
2018-11-01 13:39:37 +1100

    making an interactive component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        /*
+        * In JavaScript classes, you need to always call super when defining the constructor of a subclass.
+        * All React component classes that have a constructor should start it with a super(props) call.
+        */
+        super(props);
+        this.state = {
+            value: null,
+        }
+    }
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            /*
+            * To save typing and avoid the confusing behavior of this,
+            * we will use the arrow function syntax for event handlers here and further below.
+            * Forgetting () => and writing onClick={alert('click')} is a common mistake,
+            * and would fire the alert every time the component re-renders.
+            */
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
